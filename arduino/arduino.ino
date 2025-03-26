49385300ddcd9dc0c95bb95b129514462f184cb8
jirisitera <jirik.sitera@gmail.com>
2025-03-26 16:54:30 +0100

    updated arduino code

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -14,11 +14,11 @@ void setup() {
 void loop() {
   if (digitalRead(inputPin) == LOW) {
     digitalWrite(outputPin, HIGH);
+    if (remoteKeyboard.available() > 0) {
+      Keyboard.write((char) remoteKeyboard.read());
+    }
   }
   else {
     digitalWrite(outputPin, LOW);
-  }
-  if (remoteKeyboard.available() > 0) {
-    Keyboard.write((char) remoteKeyboard.read());
   }
 }
